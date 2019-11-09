58b1ed5e321625f4011cb8d096721b434a89447b
defintlynotabot <57571336+defintlynotabot@users.noreply.github.com>
2019-11-09 20:55:16 +0100

    Update f.py

--- f.py
+++ f.py
@@ -3,11 +3,11 @@
 #Ducky
 #09.November 2019
 #Nützliche Funktionen fürs Hauptprogramm
-from os import system, name
+from os import system, name #Name des Operating-Systems erhalten
 def clear():
-	if name == 'nt':
+	if name == 'nt': #Wenn Windows, cls- Befehl ausführen
 		_ = system('cls')
-	else:
+	else: #Unter anderen Plattformen (Linux), clear
 		_ = system('clear')
 
-from time import sleep
+from time import sleep #sleep(seconds) zulassen
